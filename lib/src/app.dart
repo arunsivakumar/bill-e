cacd5358f8b2597252011cfc14332df3b5952d90
Hernan Obera <hernan@circles.asia>
2019-07-27 21:03:23 +0800

    fix: portrait only

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:metsysoce/src/pages/home_page.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.grey,
         brightness: Brightness.dark,
-        accentColor: Colors.purpleAccent
+        accentColor: Colors.cyanAccent
       ),
       home: HomePage(),
     );
