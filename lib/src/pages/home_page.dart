6f5d5f018137c2e22254577c09c58658d6e60eef
Hernan Obera <hernan@circles.asia>
2019-07-27 16:28:45 +0800

    feat: added bills api call

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -35,11 +39,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+          child: FutureBuilder<List<Bill>>(
+            future: getBills(), //sets the getBills method as the expected Future
+            builder: (context, snapshot) {
+              if (snapshot.hasData) { //checks if the response returns valid data
+                return Center(
+                  child: Column(
+                    children: <Widget>[
+                      Text(snapshot.data.first.billerIcon), //displays the billerName
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(" - ${snapshot.data.first.billerName}"), //displays the amount
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasError) { //checks if the response throws an error
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          )
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -61,5 +82,50 @@ class _HomePageState extends State<HomePage> {
         onTap: _onItemTapped,
       ),
     );
+  }
+}
+
+class Bill {
+  final String billerId;
+  final String billerName;
+  final String billerIcon;
+  final double amount;
+  final String currency;
+  final int billDate;
+
+  Bill({this.billerId, this.billerName, this.billerIcon, this.amount,
+    this.currency, this.billDate});
+
+  factory Bill.fromJson(Map<String, dynamic> json) {
+    return Bill(
+        billerId: json['biller_id'],
+        billerName: json['biller_name'],
+        billerIcon: json['biller_icon'],
+        amount: json['amount'] as double,
+        currency: json['currency'],
+        billDate: json['bill_date']
+    );
+  }
+}
+
+List<Bill> parseBills(Map<String, dynamic> parsedJson) {
+  var billsFromJson = parsedJson['result'];
+  List<Bill> billsList = new List<Bill>();
+
+  for (var value in billsFromJson) {
+    billsList.add(Bill.fromJson(value));
+  }
+  return billsList;
+}
+
+Future<List<Bill>> getBills() async {
+  String url = 'http://192.168.0.197:4000/metsysoce/bills/id';
+  final response =
+  await http.get(url, headers: {"Accept": "application/json"});
+
+  if (response.statusCode == 200) {
+    return parseBills(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
   }
 }
